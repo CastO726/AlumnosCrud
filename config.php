70ed901d691451dd95ef01e8224c4e6535385ec4
Zahira Castillo <castillozahira26@gmail.com>
2021-12-09 16:09:42 -0800

    first commit

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'db' => [
+    'host' => 'localhost',
+    'user' => 'root',
+    'pass' => '',
+    'name' => 'alumnos',
+    'options' => [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]
+  ]
+];
